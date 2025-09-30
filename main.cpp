e721f62054956eb022107514616592032a55409d
lukasws09 <lukas.weiss@htlpinkafeld.at>
2025-09-30 09:04:39 +0200

    Add files via upload
    Das ist der src für den ESP 32

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <DHT.h>
+
+#define DHTPIN 25       // GPIO25
+#define DHTTYPE DHT11   // Sensor-Typ
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+
+  if (!isnan(temp)) {
+    Serial.print("Temperatur: ");
+    Serial.print(temp);
+    Serial.println(" °C");
+  } else {
+    Serial.println("Fehler beim Lesen vom DHT11!");
+  }
+
+  delay(10000); // nur alle 2 Sekunden messen
+}
